28b8f2a58248e200cb6bf4d77741a432f5aedc2d
Daniel Bark <danba340@gmail.com>
2020-10-05 14:54:46 +0200

    urlparams

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Board from "./Board";
+import { updateURLParameter } from "./helpers";
 
 function App() {
   const [imageUrl, setImageUrl] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has("img")) {
+      setImageUrl(urlParams.get("img"));
+    }
+  }, []);
   const handleImageChange = (event) => {
     setImageUrl(event.target.value);
+    window.history.replaceState(
+      "",
+      "",
+      updateURLParameter(window.location.href, "img", event.target.value)
+    );
   };
   return (
     <div className="App">
       <Board rows={4} cols={4} width={320} height={320} image={imageUrl} />
       <label>
         Image:
-        <input onChange={handleImageChange} type="text" name="name" />
+        <input
+          value={imageUrl}
+          onChange={handleImageChange}
+          type="text"
+          name="name"
+        />
       </label>
     </div>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -61,3 +61,23 @@ export function swap(tiles, src, dest) {
   [tilesResult[src], tilesResult[dest]] = [tilesResult[dest], tilesResult[src]];
   return tilesResult;
 }
+
+export function updateURLParameter(url, param, paramVal) {
+  var newAdditionalURL = "";
+  var tempArray = url.split("?");
+  var baseURL = tempArray[0];
+  var additionalURL = tempArray[1];
+  var temp = "";
+  if (additionalURL) {
+    tempArray = additionalURL.split("&");
+    for (var i = 0; i < tempArray.length; i++) {
+      if (tempArray[i].split("=")[0] !== param) {
+        newAdditionalURL += temp + tempArray[i];
+        temp = "&";
+      }
+    }
+  }
+
+  var rows_txt = temp + "" + param + "=" + paramVal;
+  return baseURL + "?" + newAdditionalURL + rows_txt;
+}
